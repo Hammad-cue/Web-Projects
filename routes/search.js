a1152b802ea45be453fc2dc75da84f929e4ed9bb
Hammad Khawar <zafaralisha577@gmail.com>
2026-03-30 22:43:23 +0500

    Organized projects into subfolders

--- routes/search.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../controllers/searchController');
-
-// Handles both the initial page load (empty query) and search submissions
-router.get('/', searchController.searchContent);
-
-module.exports = router;
